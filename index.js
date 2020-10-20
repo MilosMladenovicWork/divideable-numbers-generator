67b03ba1afb090a95a49d4502f59d0dd6c15e70e
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-10-20 09:40:15 +0200

    added different table organization and more functions

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const numberOfResults = 100
+const numberOfResults = 30
 
 const two1Button = document.querySelector('#two-1')
 const three1Button = document.querySelector('#three-1')
 const four1Button = document.querySelector('#four-1')
 const five1Button = document.querySelector('#five-1')
+const six1Button = document.querySelector('#six-1')
 
 const three2Button = document.querySelector('#three-2')
 const four2Button = document.querySelector('#four-2')
 const five2Button = document.querySelector('#five-2')
+
+const threeNum2Button = document.querySelector('#three-num-2')
+const threeNum3Button = document.querySelector('#three-num-3')
+const threeNum4Button = document.querySelector('#three-num-4')
+const threeNum5Button = document.querySelector('#three-num-5')
+const threeNum6Button = document.querySelector('#three-num-6')
+const threeNum7Button = document.querySelector('#three-num-7')
+const threeNum8Button = document.querySelector('#three-num-8')
+const threeNum9Button = document.querySelector('#three-num-9')
 
 const generatedNumbersTable = document.querySelector('#generated-numbers')
 
@@ -33,6 +43,10 @@ const generateRandom4Digit = () => {
 
 const generateRandom5Digit = () => {
     return randomBetweenRanges(10000, 99999)
+}
+
+const generateRandom6Digit = () => {
+    return randomBetweenRanges(100000, 999999)
 }
 
 const returnDivedablePair = (numberFunction, dividerFunction) => {
@@ -61,8 +75,8 @@ const generateFilledTable = (pair) => {
     let column2 = createColumn()
     let column3 = createColumn()
     column1.innerText = pair[0]
-    column2.innerText = pair[1]
-    column3.innerText = pair[0] / pair[1]
+    column2.innerText = '/'
+    column3.innerText = pair[1]
     row.appendChild(column1)
     row.appendChild(column2)
     row.appendChild(column3)
@@ -97,6 +111,13 @@ five1Button.addEventListener('click', function(){
     }
 })
 
+six1Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom6Digit, generateRandom1Digit)
+        generateFilledTable(pair)    
+    }
+})
+
 three2Button.addEventListener('click', function(){
     for(let i = 0; i <= numberOfResults; i++){
         let pair = returnDivedablePair(generateRandom3Digit, generateRandom2Digit)
@@ -114,6 +135,64 @@ four2Button.addEventListener('click', function(){
 five2Button.addEventListener('click', function(){
     for(let i = 0; i <= numberOfResults; i++){
         let pair = returnDivedablePair(generateRandom5Digit, generateRandom2Digit)
+        generateFilledTable(pair)    
+    }
+})
+
+threeNum2Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 2)
+        generateFilledTable(pair)    
+    }
+})
+
+threeNum3Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 3)
+        generateFilledTable(pair)    
+    }
+})
+
+threeNum4Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 4)
+        generateFilledTable(pair)    
+    }
+})
+
+threeNum5Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 5)
+        generateFilledTable(pair)    
+    }
+})
+
+threeNum6Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 6)
+        generateFilledTable(pair)    
+    }
+})
+
+
+threeNum7Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 7)
+        generateFilledTable(pair)    
+    }
+})
+
+
+threeNum8Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 8)
+        generateFilledTable(pair)    
+    }
+})
+
+threeNum9Button.addEventListener('click', function(){
+    for(let i = 0; i <= numberOfResults; i++){
+        let pair = returnDivedablePair(generateRandom3Digit, () => 9)
         generateFilledTable(pair)    
     }
 })
